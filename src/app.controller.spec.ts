baa3af3f841b2cff26182a9e9f365ec64fb24114
David Videau Ortega <videau.ortega@gmail.com>
2026-02-28 12:40:02 -0300

    feat: add nest config module globally

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,21 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AppController', () => {
   let app: TestingModule;
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
   });
 
   describe('getHello', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "test"', () => {
       const appController = app.get(AppController);
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getEnvironment()).toBe('test');
     });
   });
 });
